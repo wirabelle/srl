c9da08d6cdc5f0ab280d57452b7101f80faa1899
wirabelle <vincentrenaud55@gmail.com>
2020-01-15 15:18:55 +0000

    Fix web

--- .htrouter.php
+++ .htrouter.php
@@ -8,10 +8,10 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+if ($uri !== '/' && file_exists(__DIR__ . '/web' . $uri)) {
     return false;
 }
 
 $_GET['_url'] = $_SERVER['REQUEST_URI'];
 
-require_once __DIR__ . '/public/index.php';
+require_once __DIR__ . '/web/index.php';
